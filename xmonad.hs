5f9c937abf4b3ffdd239d4dd0bf5e389d18e3c99
Maxime Bourget <bmx007@gmail.com>
2016-03-14 21:55:01 +0000

    Add bring window action

--- xmonad.hs
+++ xmonad.hs
@@ -1,7 +1,15 @@
 import XMonad
 import XMonad.Config.Xfce
+import qualified Data.Map as M
+import XMonad.Prompt
+import XMonad.Prompt.Shell
+import XMonad.Prompt.XMonad
+import XMonad.Actions.WindowBringer
 
-main = xmonad $ xfceConfig
-  { modMask = mod4Mask
-  }
+main = xmonad $ defaultConfig { keys = myKeys } where
+  myKeys (conf@XConfig {XMonad.modMask = modm}) = M.fromList
+         [((modm, xK_g ), gotoMenu)
+         ,((modm, xK_b ), bringMenu)
+         ]
+
 
