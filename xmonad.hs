488c3cda48db3f3ed37a4393a588ad591587f76f
Maxime Bourget <bmx007@gmail.com>
2016-03-14 21:58:11 +0000

    Add Xmobar

--- xmonad.hs
+++ xmonad.hs
@@ -1,15 +1,31 @@
 import XMonad
-import XMonad.Config.Xfce
 import qualified Data.Map as M
 import XMonad.Prompt
 import XMonad.Prompt.Shell
 import XMonad.Prompt.XMonad
 import XMonad.Actions.WindowBringer
+ 
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
-main = xmonad $ defaultConfig { keys = myKeys } where
-  myKeys (conf@XConfig {XMonad.modMask = modm}) = M.fromList
-         [((modm, xK_g ), gotoMenu)
-         ,((modm, xK_b ), bringMenu)
-         ]
+main = do
+    xmproc <- spawnPipe "xmobar"
 
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , modMask = modm     -- Rebind Mod to the Windows key
+        } `additionalKeys`
+        [ ((modm, xK_g ), gotoMenu)
+        , ((modm, xK_b ), bringMenu)
+        ]
+
+    where modm = mod4Mask
 
